1f0a6c0dfd0462b3c49fb2da94551b424f7869cf
AlexKurdelska <alex.kurdelska@gmail.com>
2023-09-29 16:20:23 +0200

    Update CreateBoxRequestDto.cs

--- api/api/TransferModels/CreateBoxRequestDto.cs
+++ api/api/TransferModels/CreateBoxRequestDto.cs
@@ -5,15 +5,16 @@ namespace api.TransferModels;
 
 public class CreateBoxRequestDto
 {
-    [ValueIsOneOf(new string[] {"small", "medium"}, "Must be proper size!")]
+    [ValueIsOneOf(new string[] {"small", "medium", "big", "large"}, "Must be proper size! (small, medium, big or large)")]
     public string Size { get; set; }
     
     public int Weight { get; set; }
     public int Price { get; set; }
     
-    [MinLength(5)]
+    [MinLength(4)]
     public string Material { get; set; }
     
+    [MinLength(3)]
     public string Color { get; set; }
     public int Quantity { get; set; }
     
