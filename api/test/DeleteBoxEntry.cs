59a1e69be1ee957b4b9c05e0e746bebe0cbca6c7
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2023-10-10 18:23:11 +0200

    test for delete function UI

--- api/test/DeleteBoxEntry.cs
+++ api/test/DeleteBoxEntry.cs
@@ -2,10 +2,14 @@ using Dapper;
 using FluentAssertions;
 using FluentAssertions.Execution;
 using NUnit.Framework;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using test;
 
 namespace test;
 
-public class DeleteBoxEntry
+[TestFixture]
+public class DeleteTests : PageTest
 {
     private HttpClient _httpClient;
 
@@ -16,51 +20,95 @@ public class DeleteBoxEntry
     }
 
     [Test]
-    public async Task GetBoxTest()
+    public async Task DeleteBoxFromHttpClient()
     {
         Helper.TriggerRebuild();
-        
-            var box = new Box()
-            {
-                Id = 1,
-                Size = "small",
-                Weight = 1,
-                Price = 1,
-                Material = "plastic",
-                Color = "green",
-                Quantity = 1,
-            };
-            
-            var sql = $@"
+
+        var box = new Box()
+        {
+            Id = 1,
+            Size = "small",
+            Weight = 1,
+            Price = 1,
+            Material = "plastic",
+            Color = "green",
+            Quantity = 1,
+        };
+
+        var sql = $@"
             insert into box_factory.boxes (size, weight, price, material, color, quantity) VALUES(@size, @weight,
                 @price, @material, @color, @quantity)";
+        using (var conn = Helper.DataSource.OpenConnection())
+        {
+            conn.Execute(sql, box);
+        }
+
+        var url = "http://localhost:5000/api/boxes/1";
+        HttpResponseMessage response;
+        try
+        {
+            response = await _httpClient.DeleteAsync(url);
+            TestContext.WriteLine("THE FULL BODY RESPONSE: " + await response.Content.ReadAsStringAsync());
+        }
+        catch (Exception e)
+        {
+            throw new Exception(Helper.NoResponseMessage, e);
+        }
+
+        using (new AssertionScope())
+        {
             using (var conn = Helper.DataSource.OpenConnection())
             {
-                conn.Execute(sql, box);
+                (conn.ExecuteScalar<int>($"SELECT COUNT(*) FROM box_factory.boxes WHERE Id = 1;") == 0).Should()
+                    .BeTrue();
             }
 
-            var url = "http://localhost:5000/api/boxes/1";
-            HttpResponseMessage response;
-            try
-            {
-                response = await _httpClient.DeleteAsync(url);
-                TestContext.WriteLine("THE FULL BODY RESPONSE: " + await response.Content.ReadAsStringAsync());
-            }
-            catch (Exception e)
-            {
-                throw new Exception(Helper.NoResponseMessage, e);
-            }
+            response.IsSuccessStatusCode.Should().BeTrue();
+        }
+    }
 
-            using (new AssertionScope())
-            {
-                using (var conn = Helper.DataSource.OpenConnection())
-                {
-                    (conn.ExecuteScalar<int>($"SELECT COUNT(*) FROM box_factory.boxes WHERE Id = 1;") == 0).Should()
-                        .BeTrue();
-                }
 
-                response.IsSuccessStatusCode.Should().BeTrue();
+    [Test]
+    public async Task DeleteBoxFromUI()
+    {
+        Helper.TriggerRebuild();
+        int boxIdToDelete;
 
-            }
+
+        var box = new Box()
+        {
+            Id = 2,
+            Size = "small",
+            Weight = 10,
+            Price = 2,
+            Material = "plastic",
+            Color = "red",
+            Quantity = 10,
+        };
+
+        var sql = $@"
+            insert into box_factory.boxes (size, weight, price, material, color, quantity) VALUES(@size, @weight,
+                @price, @material, @color, @quantity)";
+        using (var conn = Helper.DataSource.OpenConnection())
+        {
+            conn.Execute(sql, box);
+            Console.WriteLine(
+                $"Box: Id={box.Id},Size={box.Size}, Weight={box.Weight}, Price={box.Price}, Material={box.Material}, Color={box.Color}, Quantity={box.Quantity}");
+        }
+
+
+        await Page.GotoAsync(Helper.ClientAppBaseUrl);
+        var card = Page.GetByTestId("card_" + box.Id);
+        var deleteButton = card.GetByTestId("ion-button[data-testid='delete_button']");
+         deleteButton.ClickAsync();
+        await Page.GotoAsync(Helper.ClientAppBaseUrl);
+        await Expect(card).Not.ToBeVisibleAsync();
+        await using (var conn = await Helper.DataSource.OpenConnectionAsync())
+
+        {
+            int count = conn.ExecuteScalar<int>("SELECT COUNT(*) FROM box_factory.boxes WHERE id=@BoxId;",
+                new { BoxId = box.Id });
+            count.Should().Be(0);
+        }
     }
 }
