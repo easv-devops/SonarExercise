13d4929a48ec443d534ebd88e2bf809a7c3bebd1
AlexKurdelska <alex.kurdelska@gmail.com>
2023-10-11 18:46:05 +0200

    Failed :(

--- api/test/CreateBox.cs
+++ api/test/CreateBox.cs
@@ -1,12 +1,14 @@
 ﻿using System.Net.Http.Json;
+using Dapper;
 using FluentAssertions;
-using FluentAssertions.Execution;
+using Microsoft.Playwright.NUnit;
 using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace test;
 
-public class CreateBox
+[TestFixture]
+public class CreateBox : PageTest
 {
     private HttpClient _httpClient;
 
@@ -16,6 +18,37 @@ public class CreateBox
         _httpClient = new HttpClient();
     }
 
+    
+    [TestCase("small", 10, 10, "wood", "green", 10)]
+    public async Task BoxCanSuccessfullyBeCreatedFromUi(string size, float weight, float price, string material, string color, int quantity)
+    {
+        //ARRANGE
+        Helper.TriggerRebuild();
+
+        //ACT
+        await Page.GotoAsync(Helper.ClientAppBaseUrl);
+        await Page.GetByTestId("createBox").ClickAsync();
+        await Page.GetByTestId("sizeInput").Locator("input").FillAsync(size);
+        await Page.GetByTestId("weightInput").Locator("input").FillAsync(weight);
+        await Page.GetByTestId("priceInput").Locator("input").FillAsync(price);
+        await Page.GetByTestId("materialInput").Locator("input").FillAsync(material);
+        await Page.GetByTestId("quantityInput").Locator("input").FillAsync(quantity);
+        await Page.GetByTestId("submit").ClickAsync();
+
+
+        //ASSERT
+        await Expect(Page.GetByTestId("card_" + )).ToBeVisibleAsync(); //Exists in UI after creation
+        await using (var conn = await Helper.DataSource.OpenConnectionAsync())
+        {
+            var expected = new Box()
+            {
+         
+            }; //Article object from test case
+
+            conn.QueryFirst<Box>("SELECT * FROM box_factory.boxes;").Should()
+                .BeEquivalentTo(expected); //Should be equal to article found in DB
+        }
+    }
 
     [Test]
     public async Task ShouldSuccessfullyCreateBox()
